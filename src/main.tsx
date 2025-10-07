d287e70972e162027082eb724df2d040a6135744
Valentin Jarysta <vjarysta@gmail.com>
2025-10-07 14:14:28 +0200

    feat: expose chooseCards debug helper

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./pages/App";
+import { registerDebugHelpers } from "./utils/debug";
+
+registerDebugHelpers();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
