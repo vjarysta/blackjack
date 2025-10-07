e504b30cc8ad8691788a09a30678783ac161dd1b
Valentin Jarysta <vjarysta@gmail.com>
2025-10-07 13:03:03 +0200

    feat: add audio feedback for gameplay events

--- src/components/animation/FlipCard.tsx
+++ src/components/animation/FlipCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { audioService } from "../../services/AudioService";
 import { ANIM, REDUCED } from "../../utils/animConstants";
 
 interface FlipCardProps {
@@ -10,6 +11,13 @@ interface FlipCardProps {
 
 export function FlipCard({ isRevealed, front, back }: FlipCardProps): React.ReactElement {
   const duration = REDUCED ? 0 : ANIM.flip.duration;
+  const wasRevealedRef = React.useRef(isRevealed);
+  React.useEffect(() => {
+    if (!wasRevealedRef.current && isRevealed) {
+      audioService.playCardFlip();
+    }
+    wasRevealedRef.current = isRevealed;
+  }, [isRevealed]);
   return (
     <div style={{ perspective: 800, position: "relative" }}>
       <motion.div
